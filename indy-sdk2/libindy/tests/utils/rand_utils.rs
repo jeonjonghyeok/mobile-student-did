5507ebe0031e3a4e93f183e60adbdbee0f823698
jeonjonghyeok <tongher1685@gmail.com>
2020-07-15 15:05:30 +0900

    dele

--- indy-sdk2/libindy/tests/utils/rand_utils.rs
+++
@@ -1,8 +0,0 @@
-extern crate rand;
-
-use self::rand::Rng;
-use self::rand::distributions::Alphanumeric;
-
-pub fn get_rand_string(len: usize) -> String {
-    rand::thread_rng().sample_iter(&Alphanumeric).take(len).collect()
-}
