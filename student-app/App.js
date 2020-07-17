69896d3707cba48160573bdcdf2599e5e5e20c3a
kimth007kim <kimth007kim@naver.com>
2020-07-17 20:23:05 +0900

    Expo document Test: Image

--- student-app/App.js
+++ student-app/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import anu from "./assets/icon.jpg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>
+      <Image source={anu} style={styles.logo} />
+      <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button
         below!
       </Text>
@@ -13,9 +15,16 @@ export default function App() {
 }
 const styles = StyleSheet.create({
   container: {
+    marginLeft: 10,
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
+  logo: {
+    width: 152,
+    height: 79,
+    marginBottom: 10,
+  },
+  instructions: { color: "#888", fontSize: 18, marginHorizontal: 15 },
 });
