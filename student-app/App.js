cffc7af55f5c9776555f2eb501604ca6db9dfed3
kimth007kim <kimth007kim@naver.com>
2020-07-20 19:29:48 +0900

    Expo document Test: Button

--- student-app/App.js
+++ student-app/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, Alert } from "react-native";
 import anu from "./assets/icon.jpg";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function App() {
   return (
@@ -10,6 +11,23 @@ export default function App() {
         To share a photo from your phone with a friend, just press the button
         below!
       </Text>
+
+      <TouchableOpacity
+        onPress={() =>
+          Alert.alert(
+            "항목을 선택해주세요",
+            "아래의 항목에서 선택해주세요",
+            [
+              { text: "OK", onPress: () => console.log("OK pressed!") },
+              { text: "Cancel", onPress: () => console.log("Cancel pressed!") },
+            ],
+            { cancelable: false }
+          )
+        }
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>선택해주세요</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -27,4 +45,10 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   instructions: { color: "#888", fontSize: 18, marginHorizontal: 15 },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: { fontSize: 20, color: "#fff" },
 });
