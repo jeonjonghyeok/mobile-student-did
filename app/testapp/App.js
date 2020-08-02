a9d1616d4eaf341be2180aa7bff5ba1c2e0a34c5
kimth007kim <kimth007kim@naver.com>
2020-08-02 16:28:14 +0900

    Expo Document Test:Configuring the header bar

--- app/testapp/App.js
+++ app/testapp/App.js
@@ -6,29 +6,42 @@ import { StatusBar } from "expo-status-bar";
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.box}>
       <Text>Home Screen</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate("Details")}
+      />
+      <Button
+        title="Profile page"
+        onPress={() =>
+          navigation.navigate("Profile", { name: "Custom profile header" })
+        }
+      />
+      <Button
+        title="Update the title"
+        onPress={() => navigation.setOptions({ title: "Updated!" })}
       />
     </View>
   );
 }
 function DetailsScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.box}>
       <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push("Details")}
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
       <Button title="Go to Home" onPress={() => navigation.goBack("Home")} />
       <Button
         title="Go back to first screen in stack with popToTop"
         onPress={() => navigation.popToTop()}
       />
+    </View>
+  );
+}
+function ProfileScreen({ navigation }) {
+  return (
+    <View style={styles.box}>
+      <Text>Profile Screen</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
@@ -38,9 +51,36 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "blue",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "My home",
+            headerStyle: { backgroundColor: "purple" },
+            headerTitleColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "normal",
+            },
+          }}
+        />
         <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={({ route }) => ({ title: route.params.name })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -50,6 +90,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
