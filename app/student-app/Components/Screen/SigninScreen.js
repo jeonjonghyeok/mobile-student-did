459a1b8c27758110aeb369410af9892c7d68aa69
kimth007kim <kimth007kim@naver.com>
2020-08-21 17:51:13 +0900

    Adding Design of DrawerNavigator and some pages.

--- app/student-app/Components/Screen/SigninScreen.js
+++ app/student-app/Components/Screen/SigninScreen.js
@@ -1,19 +1,46 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 
 /* <input type="text" placeholder="ID" />
 <input type="text" placeholder="비밀번호" /> */
 
 class SigninScreen extends Component {
+  static navigationOptions = ({ screenProps }) => ({
+    title: "회원가입",
+    headerRight: (
+      <Button onPress={() => screenProps.openDraw()} title="OPEN" color="red" />
+    ),
+  });
   render() {
     const { navigation } = this.props;
     return (
       <View style={style.container}>
-        <Button
-          title="로그인"
-          onPress={() => navigation.navigate("MainScreen")}
-        />
-        <Button title="회원가입" />
+        <View>
+          <Text>서비스를 위해 로그인 해주세요</Text>
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          <Text>ID</Text>
+          <TextInput
+            style={{ height: 40, width: 120, marginLeft: 50 }}
+            placeholder="ID"
+            backgroundColor="white"
+          />
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          <Text>PW</Text>
+          <TextInput
+            style={{ height: 40, width: 120, marginLeft: 50 }}
+            backgroundColor="white"
+            placeholder="PW"
+          />
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 100 }}>
+          <Button
+            title="로그인"
+            onPress={() => navigation.navigate("MainScreen")}
+          />
+          <Button title="회원가입" />
+        </View>
       </View>
     );
   }
@@ -25,5 +52,10 @@ const style = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  textInput: {
+    height: 40,
+    width: 120,
+    backgroundColor: "white",
   },
 });
