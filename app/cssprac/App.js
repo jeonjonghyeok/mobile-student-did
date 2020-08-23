673a73ee405873fa5f7048d61218ec21bb021c52
dsdljs <ddsdljs@naver.com>
2020-08-24 04:19:49 +0900

    SigninScreen

--- app/cssprac/App.js
+++ app/cssprac/App.js
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#1ad657',
   },
 });
-*/
+
 
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
@@ -86,7 +86,7 @@ const styles = StyleSheet.create({
 })
 
 
-/*
+
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
@@ -113,4 +113,153 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
   },
 });
+
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.item1} />
+
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    alignItems:'stretch',
+  },
+  item1: {
+    //swidth:50,
+    height:50,
+    backgroundColor: 'red',
+  },
+});
 */
+
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}><Text style={{fontSize:'25'}}>회원가입</Text></View>
+      <View style={styles.dksshd}></View>
+      <View style={styles.title}><Text style={{fontSize:'30'}}>안양대학교 모바일 학생증</Text></View>
+      <View style={styles.rodrod}>
+      <Text style={{fontSize:'20'}}>내 개인정보는 내가
+      </Text>
+      <Text style={{fontSize:'20'}}>
+        쉽고, 편하게 인증하자
+        </Text>
+      </View>
+    
+      <View style={styles.content}>
+        <Image
+          style={{height:'100%',width:'85%',resizeMode:'cover'}}
+          source={require('./hihi.png')}/>
+      </View>
+      <View style={styles.gkdnl} ><Text style={{fontSize:'40'}}> .  .  .</Text></View>
+      <View style={styles.footer}>
+        <View style={style.container2}>
+        <View style={style.dhls}>
+            <Text style={{fontSize:20}}>새로만들기</Text>
+        </View>
+        <View style={style.dhfms}>
+          <Text style={{fontSize:20}}>가져오기</Text>
+        </View>
+        </View>
+        <View>
+          
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+
+  },
+  header: {
+    width:'100%',
+    height:'7%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'steelblue',    
+  },
+  dksshd :{
+    width:"100%",
+    height:"9%",
+    //backgroundColor: "white",
+  },
+  title :{
+    width:"100%",
+    height:"7%",
+    justifyContent: "center",
+    alignItems: "center",
+    //backgroundColor: "green",
+    },
+  rodrod: {
+    width:"100%",
+    height:"10%",
+    justifyContent: "center",
+    alignItems: "center",
+    //backgroundColor: "white",
+  },
+  content : {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    //backgroundColor: "yellow",
+  },
+  gkdnl : {
+    width:"100%",
+    height:"10%",
+    justifyContent: "center",
+    alignItems:"center",
+   // backgroundColor:"red",
+  },
+  footer: {
+    width:"100%",
+    height:"20%",
+    justifyContent: "center",
+    //alignItems: "center",
+    //backgroundColor:"yellow",
+  },
+
+});
+
+const style = StyleSheet.create({
+  container2: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+
+  },
+  dhls: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center',
+  },
+  dhfms: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center',
+
+  },
+
+});
--- app/student-app/Components/Screen/SigninScreen.js
+++ app/student-app/Components/Screen/SigninScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button, TextInput } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput, Image } from "react-native";
 
 /* <input type="text" placeholder="ID" />
 <input type="text" placeholder="비밀번호" /> */
@@ -14,26 +14,39 @@ class SigninScreen extends Component {
   render() {
     const { navigation } = this.props;
     return (
-      <View style={style.container}>
+      <View style={styles.container}>
+      <View style={styles.header}><Text style={{fontSize:'25'}}>회원가입</Text></View>
+      <View style={styles.dksshd}></View>
+      <View style={styles.title}><Text style={{fontSize:'30'}}>안양대학교 모바일 학생증</Text></View>
+      <View style={styles.rodrod}>
+      <Text style={{fontSize:'20'}}>내 개인정보는 내가
+      </Text>
+      <Text style={{fontSize:'20'}}>
+        쉽고, 편하게 인증하자
+        </Text>
+      </View>
+    
+      <View style={styles.content}>
+        <Image
+          style={{height:'100%',width:'85%',resizeMode:'cover'}}
+          source={require('../../assets/hihi.png')}/>
+      </View>
+      <View style={styles.gkdnl} ><Text style={{fontSize:'40'}}> .  .  .</Text></View>
+      <View style={styles.footer}>
+        <View style={style.container2}>
+        <View style={style.dhls}>
+            <Text style={{fontSize:20}}>새로만들기</Text>
+        </View>
+        <View style={style.dhfms}>
+          <Text style={{fontSize:20}}>가져오기</Text>
+        </View>
+        </View>
         <View>
-          <Text>서비스를 위해 로그인 해주세요</Text>
+          
         </View>
-        <View style={{ flexDirection: "row", marginTop: 30 }}>
-          <Text>ID</Text>
-          <TextInput
-            style={{ height: 40, width: 120, marginLeft: 50 }}
-            placeholder="ID"
-            backgroundColor="white"
-          />
-        </View>
-        <View style={{ flexDirection: "row" }}>
-          <Text>PW</Text>
-          <TextInput
-            style={{ height: 40, width: 120, marginLeft: 50 }}
-            backgroundColor="white"
-            placeholder="PW"
-          />
-        </View>
+      </View>
+
+      
         <View style={{ flexDirection: "row", marginTop: 100 }}>
           <Button
             title="로그인"
@@ -41,21 +54,86 @@ class SigninScreen extends Component {
           />
           <Button title="회원가입" />
         </View>
-      </View>
+      </View> 
     );
   }
 }
 export default SigninScreen;
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    justifyContent: 'center',
+    backgroundColor: 'white',
+
+  },
+  header: {
+    width:'100%',
+    height:'7%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'steelblue',    
+  },
+  dksshd :{
+    width:"100%",
+    height:"9%",
+    //backgroundColor: "white",
+  },
+  title :{
+    width:"100%",
+    height:"7%",
     justifyContent: "center",
+    alignItems: "center",
+    //backgroundColor: "green",
+    },
+  rodrod: {
+    width:"100%",
+    height:"10%",
+    justifyContent: "center",
+    alignItems: "center",
+    //backgroundColor: "white",
   },
-  textInput: {
-    height: 40,
-    width: 120,
-    backgroundColor: "white",
+  content : {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    //backgroundColor: "yellow",
   },
+  gkdnl : {
+    width:"100%",
+    height:"10%",
+    justifyContent: "center",
+    alignItems:"center",
+   // backgroundColor:"red",
+  },
+  footer: {
+    width:"100%",
+    height:"20%",
+    justifyContent: "center",
+    //alignItems: "center",
+    //backgroundColor:"yellow",
+  },
+
+});
+
+const style = StyleSheet.create({
+  container2: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+
+  },
+  dhls: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center',
+  },
+  dhfms: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center',
+
+  },
+
 });
